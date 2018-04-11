22056fab5db5296ab8cb78a4e75d6607e47dd589
Steven Torrence <storrence88@gmail.com>
2018-04-11 13:09:40 -0500

    First commit

---
+++ friends.sql
@@ -0,0 +1,23 @@
+INSERT INTO `friendsdb`.`friends` (`id`, `first_name`, `last_name`, `occupation`, `created_at`, `updated_at`) 
+VALUES ('1', 'Steven', 'Torrence', 'Full Stack Developer', NOW(), NOW());
+
+SELECT *
+FROM friends;
+
+INSERT INTO `friendsdb`.`friends` (`id`, `first_name`, `last_name`, `occupation`, `created_at`, `updated_at`) 
+VALUES ('2', 'Meredith', 'Schnick', 'Math Teacher', NOW(), NOW());
+
+SELECT *
+FROM friends;
+
+INSERT INTO `friendsdb`.`friends` (`id`, `first_name`, `last_name`, `occupation`, `created_at`, `updated_at`) 
+VALUES ('3', 'Mike', 'Garcia', 'Music Teacher', NOW(), NOW());
+
+SELECT *
+FROM friends;
+
+INSERT INTO `friendsdb`.`friends` (`id`, `first_name`, `last_name`, `occupation`, `created_at`, `updated_at`) 
+VALUES ('4', 'Stephen', 'Van Hooser', 'Music Teacher', NOW(), NOW());
+
+SELECT *
+FROM friends;
